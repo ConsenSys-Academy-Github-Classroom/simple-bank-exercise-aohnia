a7b5d9b4cefa7ac9afd51574717eda3b22d843ec
aohnia <ainhoa.a@protonmail.com>
2021-09-26 18:36:16 +0200

    corrected visibility of mapping enrrolled

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -18,7 +18,7 @@ contract SimpleBank {
     // Fill in the visibility keyword
     // Hint: We want to create a getter function and allow contracts to be able
     //       to see if a user is enrolled.
-    mapping (address => bool) internal enrolled;
+    mapping (address => bool) public enrolled;
 
     // Let's make sure everyone knows who owns the bank, yes, fill in the
     // appropriate visilibility keyword
@@ -81,7 +81,7 @@ contract SimpleBank {
     // Emit the appropriate event
     function enroll() public notEnrolled returns (bool){
       // 1. enroll of the sender of this transaction     
-           enrolled[msg.sender] = true; 
+           enrolled[msg.sender] = true;  
            emit LogEnrolled(msg.sender); 
            return enrolled[msg.sender];
     }
